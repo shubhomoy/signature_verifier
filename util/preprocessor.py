a8616dc27110b2856e133bde18ec9946efc193c8
shubhomoy <shubhomoy.tubu@gmail.com>
2017-11-18 14:22:35 +0530

    reduced resolution and set threshold limit

--- util/preprocessor.py
+++ util/preprocessor.py
@@ -17,7 +17,13 @@ class SignaturePreprocessor(object):
             os.mkdir("{}/cropped".format(self.output_dir))
 
     def __crop(self, img_file):
+        """
+        Crop image according to view finder
+        :param img_file:
+        :return: cropped_image
+        """
         img = cv2.imread("{}/{}".format(self.input_dir, img_file))
+        img = cv2.resize(img, (1280, img.shape[0]*1280/img.shape[1]))
         x_center = img.shape[1] / 2
         y_center = img.shape[0] / 2
         x_lim = int((678.0 / 5312) * img.shape[1])
@@ -27,15 +33,27 @@ class SignaturePreprocessor(object):
         return cropped_image
 
     def __convert_to_binary(self, cropped_image, img_file):
+        """
+        Converts image to binary format
+        :param cropped_image:
+        :param img_file:
+        :return: binary_image
+        """
         grayscale = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2GRAY)
-        ret, thresh_img = cv2.threshold(grayscale, 120, 255, cv2.THRESH_BINARY)
+        ret, thresh_img = cv2.threshold(grayscale, 100, 255, cv2.THRESH_BINARY)
         kernel = np.ones((2, 2), np.uint8)
-        thresh_img = cv2.dilate(thresh_img, kernel, iterations=2)
-        thresh_img = np.float32(thresh_img)
+        thresh_img = cv2.erode(thresh_img, kernel, iterations=2)
         cv2.imwrite('{}/binary/{}'.format(self.output_dir, img_file), thresh_img)
         return thresh_img
 
     def __fix_rotation(self, binary_image, img_file):
+        """
+        Rotate signature if required
+        :param binary_image:
+        :param img_file:
+        :return:
+        """
+        # TODO: enhance this
         coords = np.column_stack(np.where(binary_image == 0))
         angle = cv2.minAreaRect(coords)[-1]
         if angle < -45:
@@ -53,6 +71,12 @@ class SignaturePreprocessor(object):
         return rotated
 
     def __final_crop(self, rotated_image, img_file):
+        """
+        Crop around the signature to get width and height
+        :param rotated_image:
+        :param img_file:
+        :return:
+        """
         x_arr, y_arr = np.where(rotated_image <= 0)
         x_min, x_max, y_min, y_max = min(x_arr), max(x_arr), min(y_arr), max(y_arr)
         cropped_image = rotated_image[x_min:x_max, y_min:y_max]
@@ -61,6 +85,11 @@ class SignaturePreprocessor(object):
 
     @staticmethod
     def __detect_edge(binary_image):
+        """
+        Harris algorithm to detect corners of the signature
+        :param binary_image:
+        :return:
+        """
         dst = cv2.cornerHarris(binary_image, 2, 3, 0.04)
         binary_image[:] = 0
         binary_image[dst > 0.01 * dst.max()] = 1
@@ -71,6 +100,7 @@ class SignaturePreprocessor(object):
         print 'Pre-processing'
         print '--------------------------------------'
         processed_images = []
+        pixel_list = []
         for img_file in os.listdir(self.input_dir):
             if not img_file.lower().endswith(('.png', '.jpeg', '.jpg')):
                 continue
@@ -79,8 +109,18 @@ class SignaturePreprocessor(object):
             binary_image = self.__convert_to_binary(cropped_image, img_file)
             rotated_image = self.__fix_rotation(binary_image, img_file)
             cropped_image = self.__final_crop(rotated_image, img_file)
+            pixel_list.append(len(np.where(cropped_image == 0)[0]))
             corners = self.__detect_edge(binary_image)
             processed_images.append(tuple([cropped_image, binary_image, rotated_image, cropped_image, corners, img_file]))
         print '--------------------------------------'
-        print
+        std_d = np.std(pixel_list)
+        mean = np.mean(pixel_list)
+
+        # setting threshold
+        set_threshold = mean - std_d
+        idx_to_remove = np.where(pixel_list < set_threshold)[0]
+
+        # remove the processed image which does not satisfy threshold
+        # TODO: show removed images
+        processed_images = [processed_image for i, processed_image in enumerate(processed_images) if i not in idx_to_remove]
         return processed_images
